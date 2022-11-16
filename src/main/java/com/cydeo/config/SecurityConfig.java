641a294e7c8993596153f172bb1024945033c3b6
Adelina <adelina.abdykanyeva@gmail.com>
2022-11-16 14:03:25 -0600

    Spring Security

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -20,36 +20,36 @@ import java.util.List;
 public class SecurityConfig {
 
 
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+//    @Bean
+//    public UserDetailsService userDetailsService(PasswordEncoder encoder){
 
 
 
 
-        List<UserDetails> userList = new ArrayList<>();
-
-        userList.add(
-                new User("mike", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN")) )
-        );
-        userList.add(
-                new User("ozzy", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER")) )
-        );
-
-        return new InMemoryUserDetailsManager(userList);
-
-    }
+//        List<UserDetails> userList = new ArrayList<>();
+//
+//        userList.add(
+//                new User("mike", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN")) )
+//        );
+//        userList.add(
+//                new User("ozzy", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER")) )
+//        );
+//
+//        return new InMemoryUserDetailsManager(userList);
+//
+//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         return http
                 .authorizeRequests()
-                .antMatchers("/user/**").hasAuthority("Admin")
+                .antMatchers("/user/**").hasRole("Admin")
                 .antMatchers("/project//**").hasRole("MANAGER")
                 .antMatchers("/task/employee/**").hasRole("EMPLOYEE")
                 .antMatchers("/task/**").hasRole("MANAGER")
-                .antMatchers("/task/**").hasAnyRole("EMPLOYEE", "ADMIN")
-                .antMatchers("/task/**").hasAnyAuthority("ROLE_EMPLOYEE")
+              //  .antMatchers("/task/**").hasAnyRole("EMPLOYEE", "ADMIN")
+                // .antMatchers("/task/**").hasAuthority("ROLE_EMPLOYEE")
                 .antMatchers(
                         "/",
                         "/login",
